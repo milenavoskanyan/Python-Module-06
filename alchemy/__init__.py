a13ce573072d261c70a7f641c519e846195cd691
milenavoskanyan <milena.voskanyan.ed@gmail.com>
2026-07-22 15:07:20 +0400

    2/3 done

---
+++ alchemy/__init__.py
@@ -0,0 +1,5 @@
+from alchemy.elements import create_air
+from alchemy.potions import healing_potion as heal
+from alchemy.potions import strength_potion
+
+__all__ = ["create_air", "heal", "strength_potion"]
---
+++ alchemy/transmutation/recipes.py
@@ -0,0 +1,9 @@
+from elements import create_fire
+from alchemy.elements import create_air
+from alchemy.potions import strength_potion
+
+def lead_to_gold() -> str:
+    return (
+        f"Recipe transmuting Lead to Gold: brew ’{create_air()}’"
+        f"and ’{strength_potion()}’ mixed with ’{create_fire()}’"
+    )
